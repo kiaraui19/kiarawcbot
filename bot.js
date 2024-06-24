9a910c3e0211eb7ecd144a1314848ca3fc41f8d9
kiaraui19 <kiaraui19@gmail.com>
2024-06-24 17:24:08 +0800

    bot.js

--- bot.js
+++ bot.js
@@ -122,7 +122,7 @@ client.on('guildMemberAdd', async member => {
 
       await welcomeChannel.send({ content: welcomeMessage, embeds: [embed] });
       //REMOVE BELOW LINE IF YOU DONT WANT DM MESSAGE
-      await member.send({ content: welcomeMessageDm, embeds: [embed] });
+      
   } catch (error) {
       console.error('Error sending welcome message:', error);
   }
