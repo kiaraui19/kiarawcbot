240779110563611bd341c8f032159019145365ea
kiaraui19 <kiaraui19@gmail.com>
2024-06-24 17:25:07 +0800

    bot.js

--- bot.js
+++ bot.js
@@ -107,7 +107,7 @@ client.on('guildMemberAdd', async member => {
 
       const { title, description, image, footer, color, footerURL, thumbnail, authorName, authorURL, authorIcon } = processedMessageData;
 
-      const welcomeMessage = `Hello ${member}!`;
+      const welcomeMessage = `Youkoso, ${member}!`;
 
       const welcomeMessageDm = `💝 This message has been sent from **${member.guild.name}!**`;
       const embed = new EmbedBuilder()
